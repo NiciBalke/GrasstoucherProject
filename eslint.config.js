99a4fef5b8f6089a1bddb4650acfa112f4f760ea
NiciBalke <nicolas.balke@gmail.com>
2025-09-08 16:53:28 +0200

    config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,11 @@
 // eslint.config.js
-export default {
-  env: {
-    browser: true,
-    node: true,
-    es2021: true
-  },
-  extends: ["eslint:recommended"],
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: "module"
-  },
-  rules: {}
-};
+const { defineConfig } = require("eslint/config");
+
+module.exports = defineConfig([
+	{
+		rules: {
+			semi: "error",
+			"prefer-const": "error",
+		},
+	},
+]);
