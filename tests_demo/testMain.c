af53d0494a81bec0672a9ad685b35d7803a35c80
NiciBalke <nicolas.balke@gmail.com>
2025-09-08 15:36:11 +0200

    now also sees failed tests not just error from tests

--- tests_demo/testMain.c
+++ tests_demo/testMain.c
@@ -24,26 +24,13 @@ int main()
         CU_add_test(demoSuite, tests[i].pName, tests[i].pTestFunc);
     }
 
-    /*
-    if(NULL == CU_add_test(demoSuite, "basicTest", basicTest)){
-        CU_cleanup_registry();
-        return CU_get_error();
-    }
-
-    if(NULL == CU_add_test(demoSuite, "negativeNums", negativeNums)){
-        CU_cleanup_registry();
-        return CU_get_error();
-    }
-
-    if(NULL == CU_add_test(demoSuite, "emptyArray", emptyArray)){
-        CU_cleanup_registry();
-        return CU_get_error();
-    }*/
-
     printf("hello test test");
+
+
     CU_basic_set_mode(CU_BRM_VERBOSE);
     CU_basic_run_tests();
-    CU_cleanup_registry();
 
-    return CU_get_error();
+    unsigned int failed = CU_get_number_of_failures();
+    CU_cleanup_registry();
+    return failed;
 }
